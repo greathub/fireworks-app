38ee44d55f9c0c8d1f90766e75432e13ce40504f
Jan Nawrocki <greathub@protonmail.com>
2020-12-20 22:17:13 +0100

    Create home page

--- src/app/Home/home.module.ts
+++ src/app/Home/home.module.ts
@@ -1,7 +1,7 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomePage } from './home.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
@@ -9,14 +9,15 @@ import { HomePageRoutingModule } from './home-routing.module';
 import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    HomePageRoutingModule,
-    TranslateModule
-  ],
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        ExploreContainerComponentModule,
+        HomePageRoutingModule,
+        TranslateModule,
+        ReactiveFormsModule
+    ],
   declarations: [HomePage]
 })
 export class HomePageModule {}
--- src/app/Home/home.page.ts
+++ src/app/Home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public fireworksForm: FormGroup;
+  name: string;
+  wishes: string;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.fireworksForm = this.formBuilder.group({
+      firstName: ['', Validators.compose([Validators.maxLength(30), Validators.required])],
+      wishes: ['', Validators.compose([Validators.maxLength(255), Validators.required])]
+    });
+  }
 
 }
